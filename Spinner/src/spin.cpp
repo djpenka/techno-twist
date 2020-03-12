8ab3b1bf45f461e4e4bac63b4ef62182252bf674
Dylan Penka <dylanjpenka@gmail.com>
2020-03-11 21:58:01 -0400

    Added pass/fail functionality and timer with color

--- Spinner/src/spin.cpp
+++ Spinner/src/spin.cpp
@@ -5,7 +5,6 @@
 #include <DFRobot_RGBMatrix.h>
 #include <Wire.h>
 #include <math.h>
-#include <Plotter.h>
 #include "spinner.h"
 #define OE    9
 #define LAT   10
@@ -18,35 +17,25 @@
 #define WIDTH 64
 #define HEIGHT  64
 DFRobot_RGBMatrix matrix(A, B, C, D, E, CLK, LAT, OE, false, WIDTH, HEIGHT);
-int xArray[50];
-int yArray[50];
-char xSize = 0;
-
-/*
- * RED, BLUE, GREEN, YELLOW.
- */
-int cRF = -1;
-int cLF = -1;
-int cLH = -1;
-int cRH = -1;
 
 const int BLACK = matrix.Color333(0,0,0);
 const int RED = matrix.Color333(5, 0, 0);
 const int YELLOW = matrix.Color333(5, 5, 0);
 const int BLUE = matrix.Color333(0, 0, 5);
 const int GREEN = matrix.Color333(0, 5, 0);
-const int WHITE = matrix.Color333(7, 7, 7);
+const int WHITE = matrix.Color333(5, 5, 5);
 const int PURPLE = matrix.Color333(5, 0, 5);
+const int LIGHT_GREEN = matrix.Color333(3, 6, 1);
 
-char *LH = "LEFT HAND";
-char *RH = "RIGHT HAND";
-char *LF = "LEFT FOOT";
-char *RF = "RIGHT FOOT";
+const String LH_STRING PROGMEM = "LEFT HAND";
+const String RH_STRING PROGMEM = "RIGHT HAND";
+const String LF_STRING PROGMEM = "LEFT FOOT";
+const String RF_STRING PROGMEM = "RIGHT FOOT";
 
-char *BLU = "BLUE";
-char *YEL = "YELLOW";
-char *GRE = "GREEN";
-char *REDS = "RED";
+const String BLUE_STRING  PROGMEM = "BLUE";
+const String YELLOW_STRING  PROGMEM = "YELLOW";
+const String GREEN_STRING  PROGMEM = "GREEN";
+const String RED_STRING PROGMEM = "RED";
 
 const float RSQ = sq(32.5);
 const float PI2 = PI/2;
@@ -54,160 +43,157 @@ const float START_THETA = 0;
 const float THETA_ACC = -1 * PI / 100;
 
 // Sensors
-char OUT[] = {52, 53, 50, 51, 6}; // 22 -> 6 52 -> 23
-char RIN[] = {49, 45, 41, 37, 33};
-char GIN[] = {48, 44, 40, 36, 32};
-char BBIN[] = {47, 43, 39, 35, 31};
-char YIN[] = {46, 42, 38, 34, 30};
-int threshold = 200;
-
+char OUT[] = {52, 53, 50, 51};
+char RIN[] = {48, 49, 46, 47, 44};
+char BIN_NEW[] = {45, 42, 43, 40, 41};
+char YIN_NEW[] = {38, 39, 36, 37, 34};
+char GIN_NEW[] = {35, 32, 33, 30, 31};
+int threshold = 470;
 
 CapacitiveSensor r1 = CapacitiveSensor(OUT[0], RIN[0]); 
-CapacitiveSensor r2 = CapacitiveSensor(OUT[1], RIN[1]); 
-CapacitiveSensor r3 = CapacitiveSensor(OUT[2], RIN[2]); 
-CapacitiveSensor r4 = CapacitiveSensor(OUT[3], RIN[3]); 
-CapacitiveSensor r5 = CapacitiveSensor(OUT[4], RIN[4]); 
-//
-CapacitiveSensor g1 = CapacitiveSensor(OUT[0], GIN[0]);
-CapacitiveSensor g2 = CapacitiveSensor(OUT[1], GIN[1]);
-CapacitiveSensor g3 = CapacitiveSensor(OUT[2], GIN[2]);
-CapacitiveSensor g4 = CapacitiveSensor(OUT[3], GIN[3]);
-CapacitiveSensor g5 = CapacitiveSensor(OUT[4], GIN[4]); 
-//
-CapacitiveSensor b1 = CapacitiveSensor(OUT[0], BBIN[0]);
-CapacitiveSensor b2 = CapacitiveSensor(OUT[1], BBIN[1]);
-CapacitiveSensor b3 = CapacitiveSensor(OUT[2], BBIN[2]);
-CapacitiveSensor b4 = CapacitiveSensor(OUT[3], BBIN[3]);
-CapacitiveSensor b5 = CapacitiveSensor(OUT[4], BBIN[4]); 
-//
-CapacitiveSensor y1 = CapacitiveSensor(OUT[0], YIN[0]);
-CapacitiveSensor y2 = CapacitiveSensor(OUT[1], YIN[1]);
-CapacitiveSensor y3 = CapacitiveSensor(OUT[2], YIN[2]);
-CapacitiveSensor y4 = CapacitiveSensor(OUT[3], YIN[3]);
-CapacitiveSensor y5 = CapacitiveSensor(OUT[4], YIN[4]); 
+CapacitiveSensor r2 = CapacitiveSensor(OUT[0], RIN[1]); 
+CapacitiveSensor r3 = CapacitiveSensor(OUT[0], RIN[2]); 
+CapacitiveSensor r4 = CapacitiveSensor(OUT[0], RIN[3]); 
+CapacitiveSensor r5 = CapacitiveSensor(OUT[0], RIN[4]); 
+
+CapacitiveSensor b1 = CapacitiveSensor(OUT[1], BIN_NEW[0]);
+CapacitiveSensor b2 = CapacitiveSensor(OUT[1], BIN_NEW[1]);
+CapacitiveSensor b3 = CapacitiveSensor(OUT[1], BIN_NEW[2]);
+CapacitiveSensor b4 = CapacitiveSensor(OUT[1], BIN_NEW[3]);
+CapacitiveSensor b5 = CapacitiveSensor(OUT[1], BIN_NEW[4]); 
+
+CapacitiveSensor y1 = CapacitiveSensor(OUT[2], YIN_NEW[0]);
+CapacitiveSensor y2 = CapacitiveSensor(OUT[2], YIN_NEW[1]);
+CapacitiveSensor y3 = CapacitiveSensor(OUT[2], YIN_NEW[2]);
+CapacitiveSensor y4 = CapacitiveSensor(OUT[2], YIN_NEW[3]);
+CapacitiveSensor y5 = CapacitiveSensor(OUT[2], YIN_NEW[4]); 
+
+CapacitiveSensor g1 = CapacitiveSensor(OUT[3], GIN_NEW[0]);
+CapacitiveSensor g2 = CapacitiveSensor(OUT[3], GIN_NEW[1]);
+CapacitiveSensor g3 = CapacitiveSensor(OUT[3], GIN_NEW[2]);
+CapacitiveSensor g4 = CapacitiveSensor(OUT[3], GIN_NEW[3]);
+CapacitiveSensor g5 = CapacitiveSensor(OUT[3], GIN_NEW[4]); 
 
 const int NUM_SENSORS = 20;
 long values[NUM_SENSORS];
-// Plotter p;
-
+int isTriggered[NUM_SENSORS];
 
 //DEBUG
 char debugMode = 0;
 
 // Declaring Functions
-
+void(* funckyFail)(void) = 0;
 void displayResult(int option);
-void drawSpinnerBack();
-void redrawAll();
+void drawBackground();
 void spinSpinner();
-int getBackgroundPixel(int i, int j, char inCircle);
-void addMaybe(int x, int y);
-void drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color);
+int  getBackgroundPixel(int i, int j, char inCircle);
 void sensorOut(char loopForever);
-void isActiveDebug();
+void debugRectangles();
+void debugRedraw();
+void updateIsTriggered();
+char colorIsPressed(char color);
+void slowRedrawAll();
+void printTimer(int start, int color);
 
 void setup() {
+  randomSeed(analogRead(22));
   Serial.begin(9600);
-  while(!Serial);
   matrix.begin();
   matrix.setTextColor(WHITE);
   if (debugMode) {
-    // p.Begin();
-    // p.AddTimeGraph("Test", 100, 
-    //                "R1", values[0], 
-    //                "R2", values[1], 
-    //                "R3", values[2],
-                  //  "R4", values[3],
-                      // "R5", values[4],
-    //                "G1", values[5],
-    //                "G2", values[6],
-    //                "G3", values[7],
-                  //  "G4", values[8], 
-                      // "G5", values[9],
-    //                "B1", values[10],
-    //                "B2", values[11],
-    //                "B3", values[12],
-                  //  "B4", values[13], 
-                      // "B5", values[14],
-                  //  "Y1", values[15],
-                  //  "Y2", values[16],
-                  //  "Y3", values[17],
-                  //  "Y4", values[18],
-                      // "Y5", values[19]);
     sensorOut(1);
   }
-  redrawAll();
+  drawBackground();
   delay(1000);
 }
 
 void loop() {
+  sensorOut(0);
   spinSpinner();
-  Serial.println("SpinSpinner RETURN");
-  // sensorOut(0);
-  Serial.println("SENSOR OUT COMPLETE");
   delay(100);
 }
 
-//void setPins() {
-//  int i;
-//  for (i = 50; i <= 53; i++) {
-//    pinMode(i, OUTPUT);
-//    digitalWrite(i, LOW);
-//  }
-//  pinMode(22, OUTPUT);
-//  digitalWrite(22, LOW);
-//
-//  for (i = 30; i <=49; i++) {
-//    pinMode(i, INPUT);
-//  }
-//}
-//
 void sensorOut(char loopForever) {
   char reading = 1;
+  if (debugMode) {
+    debugRectangles();
+  }
   while(reading) {
-    // values[0] = r1.capacitiveSensor(30);
-    // matrix.drawCircle(32, 32, 7, WHITE);
-    // values[5] = g1.capacitiveSensor(30);
-    // matrix.drawCircle(32, 32, 9, WHITE);
-    // values[10] = b1.capacitiveSensor(30);
-    // matrix.drawCircle(32, 32, 11, WHITE);
-    // values[15] = y1.capacitiveSensor(30);
-    // matrix.drawCircle(32, 32, 13, WHITE);
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 3; i++) {
       values[0] = r1.capacitiveSensor(30);
       values[1] = r2.capacitiveSensor(30);
       values[2] = r3.capacitiveSensor(30);
       values[3] = r4.capacitiveSensor(30);
       values[4] = r5.capacitiveSensor(30);
 
-      values[5] = g1.capacitiveSensor(30);
-      values[6] = g2.capacitiveSensor(30);
-      values[7] = g3.capacitiveSensor(30);
-      values[8] = g4.capacitiveSensor(30);
-      values[9] = g5.capacitiveSensor(30);
+      values[5] = b1.capacitiveSensor(30);
+      values[6] = b2.capacitiveSensor(30);
+      values[7] = b3.capacitiveSensor(30);
+      values[8] = b4.capacitiveSensor(30);
+      values[9] = b5.capacitiveSensor(30);
 
-      values[10] = b1.capacitiveSensor(30);
-      values[11] = b2.capacitiveSensor(30);
-      values[12] = b3.capacitiveSensor(30);
-      values[13] = b4.capacitiveSensor(30);
-      values[14] = b5.capacitiveSensor(30);
+      values[10] = y1.capacitiveSensor(30);
+      values[11] = y2.capacitiveSensor(30);
+      values[12] = y3.capacitiveSensor(30);
+      values[13] = y4.capacitiveSensor(30);
+      values[14] = y5.capacitiveSensor(30);
 
-      values[15] = y1.capacitiveSensor(30);
-      values[16] = y2.capacitiveSensor(30);
-      values[17] = y3.capacitiveSensor(30);
-      values[18] = y4.capacitiveSensor(30);
-      values[19] = y5.capacitiveSensor(30);
-
-      if (debugMode) {
-        isActiveDebug();
-        // p.Plot();
-      }
+      values[15] = g1.capacitiveSensor(30);
+      values[16] = g2.capacitiveSensor(30);
+      values[17] = g3.capacitiveSensor(30);
+      values[18] = g4.capacitiveSensor(30);
+      values[19] = g5.capacitiveSensor(30);
     }
+
+    if (debugMode) {
+      debugRedraw();
+    } else {
+      updateIsTriggered();
+    }
+    
     reading = loopForever;
   }
 }
 
-void isActiveDebug() {
+void updateIsTriggered() {
+  for (int k = 0; k < NUM_SENSORS; k++) {
+    isTriggered[k] = values[k] > threshold;
+    if (isTriggered[k]) {
+      Serial.println("IsTriggered");
+      Serial.println("k");
+      Serial.println(k);
+      Serial.println("values[k]");
+      Serial.println(values[k]);
+    }
+  }
+}
+
+void debugRedraw() {
+  for (int k = 0; k < NUM_SENSORS; k++) {
+    if (isTriggered[k] ^ (values[k] > threshold)) {
+      Serial.println(k);
+      int j = k / 5;
+      int i = k % 5;
+      int c = PURPLE;
+
+      if (values[k] > threshold) {
+        if (j == 0) {
+          c = RED;
+        } else if (j == 1) {
+          c = BLUE;
+        } else if (j == 2) {
+          c = YELLOW;
+        } else if (j == 3) {
+          c = GREEN;
+        }
+      }
+      matrix.fillRect(10 + 8 * j, 0 + 10 * i, 5, 5, c);
+      isTriggered[k] = values[k] > threshold;
+    }
+  }
+}
+
+void debugRectangles() {
   matrix.setCursor(0,0);
 
   for (int i = 0; i < 5; i++) {
@@ -216,48 +202,58 @@ void isActiveDebug() {
   }
 
   for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 4; j++) {
+    for (int j = 0; j < 4; j++) { 
       int c = PURPLE;
-      if (j == 0) {
-        c = RED;
-      } else if (j == 1) {
-        c = GREEN;
-      } else if (j == 2) {
-        c = BLUE;
-      } else if (j == 3) {
-        c = YELLOW;
-      }
-      if (values[j * 5 + i] > threshold){
-        matrix.fillRect(10 + 8 * j, 0 + 10 * i, 5, 5, c);
-      } else {
-        matrix.fillRect(10 + 8 * j, 0 + 10 * i, 5, 5, PURPLE);
-      }
+      matrix.fillRect(10 + 8 * j, 0 + 10 * i, 5, 5, c);
     }
   }
 }
 
+// 0 = Red
+// 1 = Blue
+// 2 = Yellow
+// 3 = Green
+char colorIsPressed(char color) {
+  sensorOut(0);
+  int i = color * 5;
+  for (; i < color * 5 + 5; i++) {
+    if (isTriggered[i]) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
 void displayResult(int option) {
-  char *body;
-  char *color;
+  // Option is int between 1 and 16
+  String body;
+  String color;
+  int textColor;
   int cNum = (option - 1) % 4;
+
+  // reassign cNum as workaround for proper array indexing
   if (cNum == 0) {
-    color = BLU;
+    color = RED_STRING;
+    textColor = RED;
   } else if (cNum == 1) {
-    color = YEL;
+    color = BLUE_STRING;
+    textColor = BLUE;
   } else if (cNum == 2) {
-    color = GRE;
+    color = YELLOW_STRING;
+    textColor = YELLOW;
   } else {
-    color = REDS;
+    color = GREEN_STRING;
+    textColor = GREEN;
   }
   
   if (option < 5) {
-    body = RF;
+    body = RF_STRING;
   } else if (option < 9) {
-    body = LF;
+    body = LF_STRING;
   } else if (option < 13) {
-    body = LH;
+    body = LH_STRING;
   } else {
-    body = RH;
+    body = RH_STRING;
   }
   int startY = 23; // top left
   int startX = 0;  // top left
@@ -268,33 +264,63 @@ void displayResult(int option) {
   matrix.setCursor(startX + 2, startY + 2);
   matrix.print(body);
   matrix.setCursor(startX + 2, startY + 2 + 8);
+  matrix.setTextColor(textColor);
   matrix.print(color);
+  matrix.setTextColor(WHITE);
+
+  printTimer(3, textColor);
+
+  char isPressed = colorIsPressed(cNum);
+
+  matrix.drawRect(startX , startY, 64, 16 + 3, WHITE);
+  matrix.fillRect(startX + 1, startY + 1, 62, 16 + 1, BLACK);
+
+  matrix.setCursor(startX + 2, startY + 2);
+  matrix.setTextSize(2);
+
+  if (isPressed) {
+    matrix.setTextColor(LIGHT_GREEN);
+    matrix.print(F("PASS"));
+    matrix.setTextColor(WHITE);
+  } else {
+    matrix.print(F("FAIL"));
+  }
+
+  matrix.setTextSize(1);
+
   delay(2000);
 }
 
-void drawSpinnerBack() {
-  int x, y, i;
-  for (i = 0; i < xSize; i++) {
-    matrix.drawPixel(xArray[i], yArray[i], getBackgroundPixel(xArray[i], yArray[i], 1));
+void printTimer(int start, int color) {
+  matrix.drawRect(24, 43, 16, 18, WHITE);
+  matrix.setTextSize(2);
+  matrix.setTextColor(color);
+  for (int i = start; i >= 0; i--) {
+    matrix.fillRect(25, 44, 14, 16, BLACK);
+    matrix.setCursor(27, 45);
+    matrix.print(i);
+    delay(1000);
   }
-  xSize = 0;
+  matrix.setTextSize(1);
+  matrix.setTextColor(WHITE);
 }
 
-void redrawAll() {
-  // int color;
-  // for (int i = 0; i < WIDTH; i++) {
-  //   for (int j = 0; j < HEIGHT; j++) {
-  //     color = getBackgroundPixel(i,j, 0);
-  //     matrix.drawPixel(i, j, color);
-  //   }
-  // }
-  Serial.println("REDRAW ALL!");
+
+void slowRedrawAll() {
+  int color;
+  for (int i = 0; i < WIDTH; i++) {
+    for (int j = 0; j < HEIGHT; j++) {
+      color = getBackgroundPixel(i,j, 0);
+      matrix.drawPixel(i, j, color);
+    }
+  }
+}
+
+void drawBackground() {
   uint8_t *backBuffer = matrix.backBuffer();
   for (int i = 0; i < 6144; i++) {
     backBuffer[i] = pgm_read_byte(spinner + i);
   }
-  matrix.swapBuffers(false);
-  Serial.println("REDRAW FINISH!");
 }
 
 void spinSpinner() {
@@ -303,7 +329,7 @@ void spinSpinner() {
   int endX, endY, result, finalT, t;
 
   result = random(1, 17);
-  finalTheta = result * PI / 8 + 2 * PI - PI / 16;
+  finalTheta = result * PI / 8 + 4 * PI - PI / 16;
   fTCopy = finalTheta;
   v = 0;
   finalT = 0;
@@ -315,24 +341,22 @@ void spinSpinner() {
   }
   theta = fTCopy;
   v *= -1;
-  Serial.print("NEW SPIN!");
   for (t = 0; t < finalT; t++) {
-    drawSpinnerBack();
     endX = radius * cos(theta) + 31.5;
     endY = radius * sin(theta) + 31.5;
-    drawLine(31, 31, endX, endY, BLACK);
-    drawLine(31, 32, endX, endY, BLACK);
-    drawLine(32, 31, endX, endY, BLACK);
-    drawLine(32, 32, endX, endY, BLACK);
+    drawBackground();
+    matrix.drawLine(31, 31, endX, endY, BLACK);
+    matrix.drawLine(31, 32, endX, endY, BLACK);
+    matrix.drawLine(32, 31, endX, endY, BLACK);
+    matrix.drawLine(32, 32, endX, endY, BLACK);
     delay(50);
     theta += v;
     v += THETA_ACC;
-    Serial.println("Spin!");
   }
   delay(1000);
   displayResult(result);
   
-  redrawAll();
+  drawBackground();
 }
 
 int getBackgroundPixel(int i, int j, char inCircle) {
@@ -340,7 +364,7 @@ int getBackgroundPixel(int i, int j, char inCircle) {
   int radius = 0;
   char colored = 0;
   if ((i > 29 && i < 34) || (j > 29 && j < 34)) {
-    if ((i > 29 && i < 34) && (j > 29 && j < 34)) {
+    if ((i > 29 && i < 34) && (j > 29 && j < 34) && !((i == 30 && j == 30) || (i == 33 && j == 33) || (i == 30 && j == 33) || (i == 33 && j == 30))) {
       color = BLACK;
       colored = true;
     } else if (i == 31 || j == 31 || i == 32 || j == 32) {
@@ -361,80 +385,15 @@ int getBackgroundPixel(int i, int j, char inCircle) {
     }
     if (inCircle || radius < RSQ) {
       if (theta < PI2 / 4) {
-        color = BLUE;
-      } else if (theta < PI2 / 2) {
-        color = YELLOW;
-      } else if (theta < PI2 * 3 / 4) {
-        color = GREEN;
-      } else {
         color = RED;
+      } else if (theta < PI2 / 2) {
+        color = BLUE;
+      } else if (theta < PI2 * 3 / 4) {
+        color = YELLOW;
+      } else {
+        color = GREEN;
       }
     }
   }
   return color;
 }
-
-void addMaybe(int x, int y) {
-  char present = 0;
-  for (int i = 0; i < xSize; i++) {
-    if (xArray[i] == x && yArray[i] == y) {
-      present = 1;
-      break;
-    }
-  }
-  if (!present) {
-    xArray[xSize] = x;
-    yArray[xSize] = y;
-    xSize++;
-  }
-}
-
-// Adapted from Adafruit_GFX library
-void drawLine(int16_t x0, int16_t y0, 
-          int16_t x1, int16_t y1, 
-          uint16_t color) 
-{
-  int16_t steep = abs(y1 - y0) > abs(x1 - x0);
-  if (steep) {
-    swap(x0, y0);
-    swap(x1, y1);
-  }
-
-  if (x0 > x1) {
-    swap(x0, x1);
-    swap(y0, y1);
-  }
-
-  int16_t dx, dy;
-  dx = x1 - x0;
-  dy = abs(y1 - y0);
-
-  int16_t err = dx / 2;
-  int16_t ystep;
-
-  if (y0 < y1) {
-    ystep = 1;
-  } else {
-    ystep = -1;
-  }
-
-  for (; x0<=x1; x0++) {
-    if (steep) {
-      // EDITED
-      addMaybe(y0, x0);
-      // END
-      matrix.drawPixel(y0, x0, color);
-    } else {
-      // EDITED
-      addMaybe(x0, y0);
-      // END
-      matrix.drawPixel(x0, y0, color);
-    }
-    err -= dy;
-    if (err < 0) {
-      y0 += ystep;
-      err += dx;
-    }
-  }
-}
-// End adapted from Adafruit_GFX library
